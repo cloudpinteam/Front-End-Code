a4c5cf1d54cb0325154e6c8d9df748cc88d8eeb7
YenHuang <yenhuang@lawn-128-61-119-251.lawn.gatech.edu>
2014-06-24 17:02:10 -0400

    First Commitment

---
+++ js/homePage.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  $("#upload").click(function(){
+
+    $("h1").fadeOut(500);
+    $("#icon").fadeOut(500);
+    $("#upload").fadeOut(500);
+    $(this).text("Upload more").fadeIn();
+ 	
+		
+
+
+	document.getElementById("icon").src = "img/grayIcon.png";
+    $("#icon").fadeIn(500);
+		
+
+		// $("#newImg").fadeIn(500);
+
+
+
+		// var newImage = document.createElement('img');
+		// newImage.id = "newImg";
+		// newImage.setAttribute("src", "img/grayIcon.png");
+
+		// document.body.appendChild(newImage);
+
+
+  });
+});
+
+
+
+
+
+   
+
+
+
+
+    // $("#newImage").fadeIn(500);
+
+
+   
+
+
+
+
+
+
+
