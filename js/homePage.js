0d4bca0370db92cbdcc9e4aeeec54bc51a5ede73
YenHuang <yenhuang@lawn-128-61-119-251.lawn.gatech.edu>
2014-06-25 15:30:01 -0400

    Added in a new button "Done" but some errors

--- js/homePage.js
+++ js/homePage.js
@@ -4,13 +4,22 @@ $(document).ready(function(){
     
   $("#upload").click(function(){
     $("h1").fadeOut(500);
+    $("h2").fadeOut(500);
     $("#icon").fadeOut(500);
     $("#upload").fadeOut(500);
     $(this).text("Upload more").fadeIn();
  	
 	 document.getElementById("icon").src = "img/grayIcon.png";
-    $("#icon").fadeIn(500); // this uploads the gray icon pin.
+    $("#icon").fadeIn(500); //this creates the gray icon pin
 		
+		var newButton = document.createElement('button');
+    newButton.innerHTML = "Done";
+    var buttonParent = document.getElementById("containerButton");
+		newButton.id = "done";
+
+		containerButton.appendChild(newButton);
+
+
   });
 });
 
