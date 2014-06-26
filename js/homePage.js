050a2412def3fbe003657813671d2bd42622d7e0
YenHuang <yenhuang@Yen-chiehs-MacBook-Pro.local>
2014-06-26 00:03:38 -0400

    Fixed the button issue

--- js/homePage.js
+++ js/homePage.js
@@ -7,16 +7,23 @@ $(document).ready(function(){
     $("h2").fadeOut(500);
     $("#icon").fadeOut(500);
     $("#upload").fadeOut(500);
-    $(this).text("Upload more").fadeIn();
+    // $(this).text("Upload more").fadeIn();
  	
 	 document.getElementById("icon").src = "img/grayIcon.png";
     $("#icon").fadeIn(500); //this creates the gray icon pin
 		
-		var newButton = document.createElement('button');
+
+    var newButton1 = document.createElement('button'); //this creates the "upload more" button
+    newButton1.innerHTML = "Upload more";
+    var buttonParent = document.getElementById("containerButton");
+    newButton1.id = "uploadMore";
+    containerButton.appendChild(newButton1);
+
+
+		var newButton = document.createElement('button'); // this creates the "done button"
     newButton.innerHTML = "Done";
     var buttonParent = document.getElementById("containerButton");
 		newButton.id = "done";
-
 		containerButton.appendChild(newButton);
 
 
