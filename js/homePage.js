f47d7f403062952c5202bab6c5da6006fa113273
YenHuang <yenhuang@Yen-chiehs-MacBook-Pro.local>
2014-06-26 15:42:34 -0400

    Fixed all the bugs.....

--- js/homePage.js
+++ js/homePage.js
@@ -3,22 +3,28 @@ $(document).ready(function(){
     animate({top: 50, opacity: 1}, 1200);
     
   $("#upload").click(function(){
-
     $("h1").fadeOut(500);
-
     $("h2").fadeOut(500);
-
-
     $("#icon").fadeOut(500);
     $("#upload").fadeOut(500);
-    $(this).text("Upload more").fadeIn();
- 	
-	 document.getElementById("icon").src = "img/grayIcon.png";
-    $("#icon").fadeIn(500);
-		
 
-		 $("#newImg").fadeIn(500);
+  
+   document.getElementById("icon").src = "img/grayIcon.png";
+    $("#icon").fadeIn(500); //this creates the gray icon pin
 
+
+    var newButton1 = document.createElement('button'); //this creates the "upload more" button
+    newButton1.innerHTML = "Upload more";
+    var buttonParent = document.getElementById("containerButton");
+    newButton1.id = "uploadMore";
+    buttonParent.appendChild(newButton1);
+
+
+    var newButton = document.createElement('button'); // this creates the "done button"
+    newButton.innerHTML = "Done";
+    var buttonParent = document.getElementById("containerButton");
+    newButton.id = "done";
+    buttonParent.appendChild(newButton);
 
   });
 });
@@ -34,36 +40,6 @@ function createProgressBar () {
   // Percent.class = "percent";
   // var container = document.getElementById("progress_bar");
   // progress_bar.appendChild(Percent);
-
-		// var newImage = document.createElement('img');
-		// newImage.id = "newImg";
-		// newImage.setAttribute("src", "img/grayIcon.png");
-
-		// document.body.appendChild(newImage);
-
-
-  });
-});
-
-
-
-
-
-   
-
-
-
-
-     $("#newImage").fadeIn(500);
-
-
-   
-
-
-
-
-
->>>>>>> FETCH_HEAD
 
 
 }
