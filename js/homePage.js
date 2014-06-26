d86e37d07202da46c9a7e4ffe300e9f6e4d23db1
mtmarcus <marcusthomas@Marcuss-MacBook-Pro.local>
2014-06-26 15:38:17 -0400

    Revert 12ff3f8..3de49d1
    This rolls back to commit 12ff3f8966947f45a999c41d4e4991a4427dd40e.

--- js/homePage.js
+++ js/homePage.js
@@ -3,44 +3,37 @@ $(document).ready(function(){
     animate({top: 50, opacity: 1}, 1200);
     
   $("#upload").click(function(){
-
     $("h1").fadeOut(500);
-
-    $("h2").fadeOut(500);
-
-
+    // $("h2").fadeOut(500);
     $("#icon").fadeOut(500);
     $("#upload").fadeOut(500);
-    $(this).text("Upload more").fadeIn();
+
  	
 	 document.getElementById("icon").src = "img/grayIcon.png";
-    $("#icon").fadeIn(500);
+    $("#icon").fadeIn(500); //this creates the gray icon pin
 		
 
-		 $("#newImg").fadeIn(500);
+    var newButton1 = document.createElement('button'); //this creates the "upload more" button
+    newButton1.innerHTML = "Upload more";
+    var buttonParent = document.getElementById("containerButton");
+    newButton1.id = "uploadMore";
+    buttonParent.appendChild(newButton1);
 
 
-  });
-});
+		var newButton = document.createElement('button'); // this creates the "done button"
+    newButton.innerHTML = "Done";
+    var buttonParent = document.getElementById("containerButton");
+		newButton.id = "done";
+		buttonParent.appendChild(newButton);
 
 
-function createProgressBar () {
+// function createProgressBar () {
   var Progress = document.createElement('div');
   Progress.id = "progress_bar";
   var container = document.getElementById("writtenContent");
   writtenContent.appendChild(Progress);
 
-  // var Percent = document.createElement('div');
-  // Percent.class = "percent";
-  // var container = document.getElementById("progress_bar");
-  // progress_bar.appendChild(Percent);
-
-		// var newImage = document.createElement('img');
-		// newImage.id = "newImg";
-		// newImage.setAttribute("src", "img/grayIcon.png");
-
-		// document.body.appendChild(newImage);
-
+// }
 
   });
 });
@@ -49,21 +42,5 @@ function createProgressBar () {
 
 
 
-   
 
 
-
-
-     $("#newImage").fadeIn(500);
-
-
-   
-
-
-
-
-
->>>>>>> FETCH_HEAD
-
-
-}
