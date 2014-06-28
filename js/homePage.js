def8bc8c7c197b444ee9806eea3b173705124d2e
YenHuang <yenhuang@Yen-chiehs-MacBook-Pro.local>
2014-06-27 22:44:16 -0400

    fixed the toggle switch for sign in

--- js/homePage.js
+++ js/homePage.js
@@ -26,15 +26,14 @@ $(document).ready(function(){
   });
 
 
- $('#login-trigger').click(function(){
-    $(this).next('#login-content').slideToggle();
+
+ $('#trigger').click(function(){
+    $(this).next('.dropdown-menu').slideToggle();
     $(this).toggleClass('active');          
     
     if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
       else $(this).find('span').html('&#x25BC;')
     })
-
-
 
 
 
